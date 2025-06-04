85d6f4b967e65477280f400b4b1859e4681f5413
zenonet <git@zenonet.de>
2025-06-04 22:23:41 +0200

    Added next button to feed which loads new chat

--- svelte-frontend/src/routes/feed/+page.svelte
+++ svelte-frontend/src/routes/feed/+page.svelte
@@ -1,12 +1,14 @@
-
-
 <script lang="ts">
+	import { goto, invalidate, invalidateAll } from '$app/navigation';
 	import type { Chat } from '$lib/chat';
 	import ChatView from '../../ChatView.svelte';
 	import ChatViewerWithDetails from '../../ChatViewerWithDetails.svelte';
 
-    // imported from load function
-    export let data: Chat;
+	// imported from load function
+	export let data: Chat;
 </script>
 
-<ChatViewerWithDetails chat={data}></ChatViewerWithDetails>
+<div style="display: flex; flex-direction: column; align-items: center; min-height: 80vh">
+	<ChatViewerWithDetails chat={data}></ChatViewerWithDetails>
+	<button style="margin-top: auto" onclick={async () => await invalidateAll()}>Next Chat</button>
+</div>
